37b88ad5dac6e16d82c3dd5f27d1b98b1ea0604f
Mirko <mirkocaccavone03@gmail.com>
2025-02-18 14:46:46 +0100

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const posts = [
+    { id: 1, title: "Introduzione a JavaScript" },
+    { id: 2, title: "Guida a Node.js" },
+    { id: 3, title: "Express: Creare un server" },
+    { id: 4, title: "CSS Flexbox e Grid" },
+    { id: 5, title: "Ottimizzazione delle performance web" },
+    { id: 6, title: "Async/Await in JavaScript" },
+    { id: 7, title: "REST API con Express e MongoDB" },
+    { id: 8, title: "Introduzione a React" },
+    { id: 9, title: "State Management con Redux" },
+    { id: 10, title: "Autenticazione con JWT e Node.js" }
+];
